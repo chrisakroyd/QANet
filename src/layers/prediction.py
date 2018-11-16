881b59da2c1269fcbb13860b16cea84a134237f1
Christopher Akroyd <chris@chrisakroyd.com>
2018-11-16 19:27:48 +0000

    Fixed bug where we couldn't predict pointers for contexts smaller than the answer limit.

--- src/layers/prediction.py
+++ src/layers/prediction.py
@@ -6,7 +6,7 @@ class PredictionHead(Layer):
     def __init__(self, answer_limit, **kwargs):
         """ Prediction Head
 
-            The prediction head simply computes the 'best' valid start and end pointer using an upper trainglular
+            The prediction head simply computes the 'best' valid start and end pointer using an upper triangular
             matrix.
 
             Args:
@@ -22,17 +22,21 @@ class PredictionHead(Layer):
 
     def call(self, x, training=None, mask=None):
         start_logits, end_logits = x
+        lower = tf.cast(0, dtype=tf.int32)
+        # Allows us to compute start/end pointers when we have a context length < answer limit.
+        upper = tf.math.minimum(self.answer_limit, tf.shape(start_logits)[1])
 
-        p_start = self.start_softmax(start_logits)
-        p_end = self.end_softmax(end_logits)
+        start_prob = self.start_softmax(start_logits)
+        end_prob = self.end_softmax(end_logits)
 
-        outer = tf.matmul(tf.expand_dims(p_start, axis=2),
-                          tf.expand_dims(p_end, axis=1))
+        outer = tf.matmul(tf.expand_dims(start_prob, axis=2),
+                          tf.expand_dims(end_prob, axis=1))
 
-        outer = tf.matrix_band_part(outer, 0, self.answer_limit)
+        outer = tf.matrix_band_part(outer, lower, upper)
         start_pointer = tf.argmax(tf.reduce_max(outer, axis=2), axis=1)
         end_pointer = tf.argmax(tf.reduce_max(outer, axis=1), axis=1)
-        return start_pointer, end_pointer
+        return start_prob, end_prob, start_pointer, end_pointer
 
     def compute_output_shape(self, input_shape):
+        # @TODO @cakroyd fix the output shape so its calculated correctly (This is wrong as of 16/11/2018).
         return (input_shape[0][0], 1), (input_shape[-1][0], 1)
