cdaaa00c0e7c775ebb014f8cec0dccd3f0eeda59
Christopher Akroyd <chris@chrisakroyd.com>
2019-01-02 19:11:46 +0000

    Package updates + switched prod build to use terser.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,15 @@
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'production',
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
     new CleanWebpackPlugin(['dist'], { verbose: true }),
-    new UglifyJSPlugin({
-      sourceMap: true,
-    }),
   ],
 });
