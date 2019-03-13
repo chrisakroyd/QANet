36fb497a06f14d24e6f652b3885dad4915bb3456
Christopher Akroyd <chris@chrisakroyd.com>
2019-03-13 16:35:47 +0000

    Character conv works best with default init (glorot_uniform)

--- src/models/embedding_layer.py
+++ src/models/embedding_layer.py
@@ -72,8 +72,7 @@ class EmbeddingLayer(tf.keras.Model):
                                                                                        verify_shape=True),
                                         name='char_embedding')
 
-        self.char_conv = Conv1D(char_dim, kernel_size=kernel_size, activation='relu', padding='same', name='char_conv',
-                                kernel_initializer=layers.create_initializer(),)
+        self.char_conv = Conv1D(char_dim, kernel_size=kernel_size, activation='relu', padding='same', name='char_conv')
 
         self.word_dropout = Dropout(word_dropout)
         self.char_dropout = Dropout(char_dropout)
