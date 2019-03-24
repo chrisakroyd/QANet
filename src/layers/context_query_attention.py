3872a96ffe9b1fd3e9d281a8b6ec83a14c7eaa4d
Christopher Akroyd <chris@chrisakroyd.com>
2019-03-24 11:41:35 +0000

    Documentation update.

--- src/layers/context_query_attention.py
+++ src/layers/context_query_attention.py
@@ -34,6 +34,8 @@ class ContextQueryAttention(Layer):
         """ Adds the necessary weights and an optional bias variable """
         hidden_size = int(input_shape[0][-1])
 
+        # We use the same initialization as the self attention/feed forward, this init works better because of the
+        # massive use of layer norm, if no layer norm its best to use a more standard weight init scheme.
         self.W0 = self.add_weight(name='W0',
                                   shape=(hidden_size, 1),
                                   initializer=layers.create_initializer(),
