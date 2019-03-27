ad7e5bf7bfbe0c14f73e0bee66dd247a2a14bf4d
Christopher Akroyd <chris@chrisakroyd.com>
2019-03-27 15:34:20 +0000

    Minor refactoring.

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,2 +1,3 @@
 from .squad import process as squad_process, fit_and_extract
 from .text import normalize, normalize_answer, text_span, clean, is_dash
+from .util import write_tf_record, create_record, get_examples, convert_to_indices
