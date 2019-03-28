289b03264fbc0233ca645a06c011defac403b6f7
Christopher Akroyd <chris@chrisakroyd.com>
2019-03-28 15:53:00 +0000

    Bias support for multi-head attention.

--- src/layers/multi_head_attention.py
+++ src/layers/multi_head_attention.py
@@ -4,7 +4,7 @@ from src import layers
 
 
 class MultiHeadAttention(Layer):
-    def __init__(self, hidden_size=128, num_heads=8, dropout=0.1, **kwargs):
+    def __init__(self, hidden_size=128, num_heads=8, dropout=0.1, use_bias=False, **kwargs):
         """ Multi-Headed Attention implementation.
 
             This is an implementation of multi-head attention based on the paper "Attention
@@ -22,6 +22,8 @@ class MultiHeadAttention(Layer):
                 hidden_size: The number of units in the final dimension of the input tensor.
                 num_heads: Number of attention heads to compute, when num_heads == 1, multi-head attention == attention
                 dropout: Fraction of units to drop.
+                use_bias: Whether or not to use bias on the linear transformations, in Attention is All You Need no bias
+                          is used but in BERT, GPT and GPT-2 it is.
                 self_attention: Boolean value for whether to use self-attention on the inputs.
         """
         super(MultiHeadAttention, self).__init__(**kwargs)
@@ -29,16 +31,13 @@ class MultiHeadAttention(Layer):
         self.num_heads = num_heads
         self.hidden_size = hidden_size
 
-        self.queries_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=False,
+        self.queries_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=use_bias,
                                     kernel_initializer=layers.create_initializer())
-
-        self.keys_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=False,
+        self.keys_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=use_bias,
                                  kernel_initializer=layers.create_initializer())
-
-        self.values_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=False,
+        self.values_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=use_bias,
                                    kernel_initializer=layers.create_initializer())
-
-        self.output_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=False,
+        self.output_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=use_bias,
                                    kernel_initializer=layers.create_initializer())
 
         if not self.hidden_size % self.num_heads == 0:
