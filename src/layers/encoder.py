Fixed accuracy hit with gradient checkpointing due to non-deterministic dropout.

src/layers/encoder.py
@@ -6,7 +6,7 @@ from src import layers
 class EncoderBlock(tf.keras.Model):
     def __init__(self, conv_layers, kernel_size, block_number=0, total_blocks=1,
                  hidden_size=128, heads=8, dropout=0.1, attn_dropout=0.1, ff_inner_size=1.0,
-                 recompute_gradients=False, **kwargs):
+                 recompute=False, **kwargs):
         """ Builds an encoder block.
 
             Encoder block from the paper "QANet" (https://arxiv.org/pdf/1804.09541.pdf, section 2.2), it is roughly
@@ -29,7 +29,7 @@ class EncoderBlock(tf.keras.Model):
                 heads: Number of attention heads to use.
                 dropout: Fraction of input units to drop in all dropout layers within this block.
                 ff_inner_size: Number of units in the inner non-linear layer of the feed-forward block.
-                recompute_gradients: Whether or not to recompute the output of the multi-head attention layer
+                recompute: Whether or not to recompute the output of the multi-head attention layer
                     during back-propagation to save GPU Memory.
         """
         super(EncoderBlock, self).__init__(**kwargs)
@@ -53,16 +53,17 @@ class EncoderBlock(tf.keras.Model):
 
         # We need to wrap the layers we with a variable scope to 100% ensure that it only recomputes those values
         # but this breaks backwards compatibility on checkpoints. Therefore only wrap if we are recomputing
-        if recompute_gradients:
+        if recompute:
             with tf.variable_scope('self_attention_%d' % self.self_attention_id, reuse=tf.AUTO_REUSE):
-                self.multi_head = layers.MultiHeadAttention(hidden_size, num_heads=heads, dropout=attn_dropout)
+                self.multi_head = layers.MultiHeadAttention(hidden_size, num_heads=heads, dropout=attn_dropout,
+                                                            recompute=recompute)
         else:
             self.multi_head = layers.MultiHeadAttention(hidden_size, num_heads=heads, dropout=attn_dropout)
 
         self.self_attention = layers.SublayerWrapper(self.multi_head,
                                                      use_layer_dropout=False,
                                                      dropout=dropout,
-                                                     recompute_gradients=recompute_gradients,
+                                                     recompute_gradients=recompute,
                                                      name='self_attention_%d' % self.self_attention_id)
 
         self.feed_forward = layers.SublayerWrapper(layers.FeedForwardLayer(hidden_size, inner_size=ff_inner_size,

src/layers/multi_head_attention.py
@@ -4,7 +4,7 @@ from src import layers
 
 
 class MultiHeadAttention(Layer):
-    def __init__(self, hidden_size=128, num_heads=8, dropout=0.1, use_bias=False, **kwargs):
+    def __init__(self, hidden_size=128, num_heads=8, dropout=0.1, use_bias=False, recompute=False, **kwargs):
         """ Multi-Headed Attention implementation.
 
             This is an implementation of multi-head attention based on the paper "Attention
@@ -24,12 +24,15 @@ class MultiHeadAttention(Layer):
                 dropout: Fraction of units to drop.
                 use_bias: Whether or not to use bias on the linear transformations, in Attention is All You Need no bias
                           is used but in BERT, GPT and GPT-2 it is.
+                recompute: Whether or not we are recomputing the gradients on this layer, if true we skip
+                           non-deterministic ops like dropout.
                 self_attention: Boolean value for whether to use self-attention on the inputs.
         """
         super(MultiHeadAttention, self).__init__(**kwargs)
         self.supports_masking = True
         self.num_heads = num_heads
         self.hidden_size = hidden_size
+        self.recompute = recompute
 
         self.queries_layer = Conv1D(self.hidden_size, kernel_size=1, use_bias=use_bias,
                                     kernel_initializer=layers.create_initializer())
@@ -86,7 +89,6 @@ class MultiHeadAttention(Layer):
             x, y = x
         else:  # Invalid, we can only take a tensor or list of two tensors for attention.
             raise ValueError('Expected a maximum of two tensors passed to multi-head attention, got: {}'.format(len(x)))
-
         batch_size, length_x = self.compute_input_shape(x)
         _, length_y = self.compute_input_shape(y)
         query, key, values = self.queries_layer(x), self.keys_layer(y), self.values_layer(y)
@@ -108,7 +110,11 @@ class MultiHeadAttention(Layer):
                 raise ValueError('Expected mask dtype to be tf.float32 or tf.bool')
 
         weights = self.softmax(logits)
-        weights = self.dropout(weights, training=training)
+
+        # Dropout is random -> Avoid when recomputing gradients as can't work out a way to make it deterministic.
+        if not self.recompute:
+            weights = self.dropout(weights, training=training)
+
         attention = tf.matmul(weights, values)
 
         attention = self.combine_heads(attention, batch_size, length_x)  # [batch_size, length_x, hidden_size]

src/models/qanet_contextual.py
@@ -33,7 +33,7 @@ class QANetContextual(tf.keras.Model):
                                                           dropout=self.dropout,
                                                           attn_dropout=self.attn_dropout,
                                                           ff_inner_size=params.ff_inner_size,
-                                                          recompute_gradients=params.low_memory,
+                                                          recompute=params.low_memory,
                                                           name='embedding_encoder')
 
         self.context_query = layers.ContextQueryAttention(name='context_query_attention')
@@ -46,7 +46,7 @@ class QANetContextual(tf.keras.Model):
                                                       dropout=self.dropout,
                                                       attn_dropout=self.attn_dropout,
                                                       ff_inner_size=params.ff_inner_size,
-                                                      recompute_gradients=params.low_memory,
+                                                      recompute=params.low_memory,
                                                       name='model_encoder')
 
         self.start_output = layers.OutputLayer(name='start_logits', use_bias=params.output_bias)